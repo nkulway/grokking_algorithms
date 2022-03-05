331d6cd7ec9a2035d5c3dc9b4eca2d03ad8406c3
Nick Kulway <75750859+nkulway@users.noreply.github.com>
2022-03-05 11:48:39 -0500

    Update 04_recursive_max.js
    Updated syntax to ES6

--- 04_quicksort/javascript/04_recursive_max.js
+++ 04_quicksort/javascript/04_recursive_max.js
@@ -6,7 +6,7 @@
  * @param {Array} array Array of numbers
  * @returns {number} The argest number
  */
-function max(array) {
+let max = array => {
   if (array.length === 2) return array[0] > array[1] ? array[0] : array[1];
   let sub_max = max(array.slice(1));
   return array[0] > sub_max ? array[0] : sub_max;
@@ -19,7 +19,7 @@ function max(array) {
  * @param {number} max Maximum value
  * @returns {number} The argest number
  */
-function alternativeSolutionMax(array, max = 0) {
+let alternativeSolutionMax = (array, max = 0) => {
   return array.length === 0
     ? max
     : alternativeSolutionMax(array.slice(1), array[0] > max ? array[0] : max);
