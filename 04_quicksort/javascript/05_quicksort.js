40bd4f68a02d61ec01f6c5826125839392df8ac6
Nick Kulway <75750859+nkulway@users.noreply.github.com>
2022-03-05 12:53:54 -0500

    Update 05_quicksort.js
    Updated syntax to ES6

--- 04_quicksort/javascript/05_quicksort.js
+++ 04_quicksort/javascript/05_quicksort.js
@@ -5,17 +5,17 @@
  * @param {Array} array Source array
  * @returns {Array} Sorted array
  */
-function quicksort(array) {
+let quicksort = array {
   // base case, arrays with 0 or 1 element are already "sorted"
   if (array.length < 2) return array;
   // recursive case
   let pivot = array[0];
   // sub-array of all the elements less than the pivot
-  let less = array.slice(1).filter(function(el) {
+  let less = array.slice(1).filter(el => {
     return el <= pivot;
   });
   // sub-array of all the elements greater than the pivot
-  let greater = array.slice(1).filter(function(el) {
+  let greater = array.slice(1).filter(el => {
     return el > pivot;
   });
   return quicksort(less).concat([pivot], quicksort(greater));
